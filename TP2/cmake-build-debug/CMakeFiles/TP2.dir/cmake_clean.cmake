3c0ea06ba122ee631ee7ef3c884a897c1975e583
dcassinotti <damiancassinotti.33@gmail.com>
2017-09-17 13:18:32 -0300

    TP" - CLion shit

---
+++ TP2/cmake-build-debug/CMakeFiles/TP2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP2.dir/src/EchoProcessor.cpp.o"
+  "CMakeFiles/TP2.dir/src/LineProcessor.cpp.o"
+  "CMakeFiles/TP2.dir/src/main.cpp.o"
+  "CMakeFiles/TP2.dir/src/test.cpp.o"
+  "TP2.pdb"
+  "TP2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
